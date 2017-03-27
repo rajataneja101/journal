bc36e125f9bfe5047f74500b7d2915a4a8daa238
rajataneja101 <tanejarajat101@gmail.com>
2017-03-27 13:27:27 +0530

    Maths Project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <title>MySQL file upload example</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <h1>Welcome</h1>
+    <a href="login.php">Login Here!</a>
+    <a href="register.php">Register here!</a>
+</body>
+</html>
